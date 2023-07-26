c2fad3e65c182ca75a10dd0e617b7693ac3b66b1
PhilLanzetta <71086048+PhilLanzetta@users.noreply.github.com>
2023-07-26 17:03:27 -0400

    ticker speed

--- src/components/ticker.js
+++ src/components/ticker.js
@@ -3,7 +3,7 @@ import Marquee from "react-fast-marquee"
 
 const Ticker = () => {
   return (
-    <Marquee className="word-marquee" gradient={false} speed={20}>
+    <Marquee className="word-marquee" gradient={false} speed={40}>
       {[...Array(10)].map((e, i) => (
         <div className="marquee-item" key={i}>
           Website Coming Soon
